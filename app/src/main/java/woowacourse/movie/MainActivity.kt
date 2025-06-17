228d16a7198f87e7ceabeb9a16e1d446e215a39b
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-06-17 11:30:04 +0900

    feat: 영화 목록 화면에서 영화 예매 완료 화면으로 영화 정보 넘기는 intent 구현

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -5,10 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import woowacourse.movie.ticket.Summary
+import woowacourse.movie.uiModel.MovieIntentModel
+import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initView()
+        val movieUiModel = MovieIntentModel(
+            name = "해리포터와 마법사의 돌",
+            screeningDate = LocalDate.of(2025, 4, 1)
+        )
+        val intent = Summary.intent(this, movieUiModel)
+        startActivity(intent)
+    }
+
+    private fun initView() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
--- app/src/main/java/woowacourse/movie/ticket/Summary.kt
+++ app/src/main/java/woowacourse/movie/ticket/Summary.kt
@@ -1,21 +1,38 @@
 package woowacourse.movie.ticket
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import woowacourse.movie.R
+import woowacourse.movie.uiModel.MovieIntentModel
 
 class Summary : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_summary)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.summary)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    companion object {
+        private const val SUMMARY_INTENT_KEY = "summary"
+
+        fun intent(context: Context, movie: MovieIntentModel): Intent {
+            val intent = Intent(context, Summary::class.java)
+                .putExtra(SUMMARY_INTENT_KEY, movie)
+            return intent
         }
     }
 }
