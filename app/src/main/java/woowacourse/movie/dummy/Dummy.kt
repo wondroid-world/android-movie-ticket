89cad0cda8a4df172114f7bd99cd4d91dfe8ad77
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-06-18 10:27:59 +0900

    feat: dummy 데이터 생성

---
+++ app/src/main/java/woowacourse/movie/dummy/Dummy.kt
@@ -0,0 +1,16 @@
+package woowacourse.movie.dummy
+
+import woowacourse.movie.R
+import woowacourse.movie.domain.Movie
+import java.time.LocalDate
+
+object Dummy {
+    val data: List<Movie> = listOf(
+        Movie(
+            title = "해리 포터와 마법사의 돌",
+            poster = R.drawable.harry_potter_and_the_sorcerers_stone,
+            screeningDate = LocalDate.of(2025, 4, 1),
+            runningTime = 152
+        )
+    )
+}
