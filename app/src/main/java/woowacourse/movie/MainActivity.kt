50faecc66d04b433bfae9e4e4e20ed7bb38eb8c0
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-06-18 14:01:31 +0900

    feat: 영화 목록에는 하나의 영화가 보인다.

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -1,6 +1,7 @@
 package woowacourse.movie
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,18 +9,14 @@ import androidx.core.view.WindowInsetsCompat
 import woowacourse.movie.dummy.Dummy
 import woowacourse.movie.view.ticket.Summary
 import woowacourse.movie.intentmodel.MovieIntentModel
+import woowacourse.movie.uimodel.MovieUiModel
 import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
-        val movieIntentModel = MovieIntentModel(
-            id = Dummy.data[0].id,
-            name = Dummy.data[0].title,
-            screeningDate = Dummy.data[0].screeningDate
-        )
-        moveOtherView(movieIntentModel)
+        initAdapter()
     }
 
     private fun initView() {
@@ -35,5 +32,34 @@ class MainActivity : AppCompatActivity() {
     private fun moveOtherView(movieIntentModel: MovieIntentModel) {
         val intent = Summary.intent(this, movieIntentModel)
         startActivity(intent)
+    }
+
+    private fun initAdapter() {
+        val movies = changeUiModel()
+        val adapter = MoviesAdapter(movies, bookMovie())
+        val moviesView = findViewById<ListView>(R.id.main_movies)
+        moviesView.adapter = adapter
+    }
+
+    private fun changeUiModel(): List<MovieUiModel> {
+        val movies = Dummy.data.map { movie ->
+            MovieUiModel(
+                id = movie.id,
+                title = movie.title,
+                poster = movie.poster,
+                screeningDate = movie.screeningDate,
+                runningTime = movie.runningTime,
+            )
+        }
+        return movies
+    }
+
+    private fun bookMovie(): (MovieUiModel) -> Unit = { movie ->
+        val movieIntentModel = MovieIntentModel(
+            id = movie.id,
+            name = movie.title,
+            screeningDate = movie.screeningDate
+        )
+        moveOtherView(movieIntentModel)
     }
 }
---
+++ app/src/main/java/woowacourse/movie/MoviesAdapter.kt
@@ -0,0 +1,55 @@
+package woowacourse.movie
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import woowacourse.movie.uimodel.MovieUiModel
+
+class MoviesAdapter(
+    private val movies: List<MovieUiModel>,
+    private val bookMovie: (MovieUiModel) -> Unit,
+) : BaseAdapter() {
+    override fun getCount(): Int = movies.size
+
+    override fun getItem(position: Int): MovieUiModel = movies[position]
+
+    // ListView는 내부적으로 아이템을 구분할 때 이 ID를 기준으로 처리
+    override fun getItemId(position: Int): Long = movies[position].id
+
+    // 리스트 순서가 바뀌어도 아이다가 그대로임을 보장 -> ListView는 그대로 재사용함
+    override fun hasStableIds(): Boolean = true
+
+    override fun getView(
+        position: Int,
+        convertView: View?,
+        parent: ViewGroup
+    ): View {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+        val movie = getItem(position)
+
+        val title = view.findViewById<TextView>(R.id.textView_main_movie_title)
+        val screeningDate = view.findViewById<TextView>(R.id.textView_main_movie_screening_date)
+        val runningTime = view.findViewById<TextView>(R.id.textView_main_movie_running_time)
+        val poster = view.findViewById<ImageView>(R.id.imageView_main_movie_poster)
+        val button = view.findViewById<Button>(R.id.button_main_movie_book)
+        title.text = movie.title
+        screeningDate.text = parent.context.getString(
+            R.string.movie_screening_date,
+            movie.screeningDate.year,
+            movie.screeningDate.monthValue,
+            movie.screeningDate.dayOfMonth
+        )
+        runningTime.text =
+            parent.context.getString(R.string.movie_running_time, movie.runningTime)
+        movie.posterImage(parent.context).load(poster, parent.context)
+        button.setOnClickListener {
+            bookMovie(movie)
+        }
+
+        return view
+    }
+}
