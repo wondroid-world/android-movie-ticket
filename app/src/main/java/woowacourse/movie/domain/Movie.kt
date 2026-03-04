3b77f83e429a62d0dc7e3aa3ca8d420ac9b1f204
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2026-03-04 13:18:36 +0900

    feat: 도메인의 id 추가

--- app/src/main/java/woowacourse/movie/domain/Movie.kt
+++ app/src/main/java/woowacourse/movie/domain/Movie.kt
@@ -3,7 +3,7 @@ package woowacourse.movie.domain
 import java.time.LocalDate
 
 data class Movie(
-    val id: Int,
+    val id: Long,
     val title: String,
     val showtime: LocalDate,
     val runningTime: Int,
